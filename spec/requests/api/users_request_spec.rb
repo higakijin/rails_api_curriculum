d13c0153730e5861c4aa215dc7fe9ddd28fae67f
higakijin <hm385.chejptks@gmail.com>
2021-10-24 11:45:09 +0900

    finish making system

---
+++ spec/requests/api/users_request_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "Api::Users", type: :request do
+  describe 'POST /users' do
+    let(:user_params) { { user:  { name: 'higakijin', email: 'hm385.chejptks@gmail.com', password: '123456', password_confirmation: '123456'}}}
+    it 'ユーザーが作成できること' do
+      post api_users_path, params: user_params
+      except(response).to have_http_status(200)
+      json = JSON.parse(response.body)
+      except(json['user']).to include({
+        'id' => be_present,
+        'name' => 'higakijin',
+        'email' => 'hm385.chejptks@gmail.com'
+      })
+    end
+
+    let(:invalid_user_params) { { user: { name: 'higakijin', email: 'hm385.chejptks@gmail.com', password: '123456', password_confirmation: '1234' } } }
+    it 'ユーザーの作成に失敗すること' do
+      post api_users_path, params: invalid_user_params
+      except(response).to have_http_status(422)
+      json = JSON.parse(response.body)
+      except(json['error']).to be_present
+    end
+  end
+end
